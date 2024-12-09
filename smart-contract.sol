07bbe2ed12309595d9171500869a64fb9a266581
Janek <62665388+Janek1010@users.noreply.github.com>
2024-12-09 20:58:30 +0100

    Update smart-contract.sol

--- smart-contract.sol
+++ smart-contract.sol
@@ -4,16 +4,13 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract SensorToken is ERC20 {
-    address public admin;
     mapping(address => uint256) public sensorBalances;
 
-    constructor() ERC20("SensorToken", "SENS") {
-        admin = msg.sender;
-    }
+    constructor() ERC20("SensorToken", "SENS") {}
 
+    // Funkcja nagradzania sensora - może ją wywołać każdy użytkownik
     function rewardSensor(address sensor, uint256 amount) external {
-        require(msg.sender == admin, "Only admin can reward sensors");
-        _mint(sensor, amount);
+        _mint(sensor, amount); // Mintowanie tokenów dla sensora
         sensorBalances[sensor] += amount;
     }
 
@@ -22,8 +19,11 @@ contract SensorToken is ERC20 {
         return balanceOf(sensor);
     }
 
-    // Możliwość przeglądania stanu tokenów dla sensora przez administratora
+    // Możliwość przeglądania stanu tokenów dla sensora przez każdego użytkownika
     function getSensorBalance(address sensor) external view returns (uint256) {
         return sensorBalances[sensor];
     }
 }
+// adres tokenu: 0x3D9042d5e322693b2208AEC319E83D0992e829F8
+// siec: https://holesky.etherscan.io/
+// paliwko: https://cloud.google.com/application/web3/faucet/ethereum/holesky
